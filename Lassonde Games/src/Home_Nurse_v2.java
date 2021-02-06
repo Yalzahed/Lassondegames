226e1d6fc54b2c3269c6391282200138cb949364
Nagham Sabbour <Nagham.sabbour@gmail.com>
2021-02-06 10:41:38 +0300

    scaled buttons

--- Lassonde Games/src/Home_Nurse_v2.java
+++ Lassonde Games/src/Home_Nurse_v2.java
@@ -12,6 +12,8 @@ import javax.swing.JScrollBar;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import javax.swing.JScrollPane;
+import javax.swing.Box;
 
 public class Home_Nurse_v2 extends JFrame {
 
@@ -38,78 +40,73 @@ public class Home_Nurse_v2 extends JFrame {
 	 */
 	public Home_Nurse_v2() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 500);
+		setBounds(100, 100, 1500, 800);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{776, 0};
-		gbl_contentPane.rowHeights = new int[]{48, 50, 278, 71, 0};
-		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
-		contentPane.setLayout(gbl_contentPane);
+		contentPane.setLayout(null);
 		
 		JPanel panel_2 = new JPanel();
-		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
-		gbc_panel_2.anchor = GridBagConstraints.NORTH;
-		gbc_panel_2.fill = GridBagConstraints.HORIZONTAL;
-		gbc_panel_2.insets = new Insets(0, 0, 5, 0);
-		gbc_panel_2.gridx = 0;
-		gbc_panel_2.gridy = 0;
-		contentPane.add(panel_2, gbc_panel_2);
+		panel_2.setBounds(5, 5, 1273, 48);
+		contentPane.add(panel_2);
+		panel_2.setLayout(null);
 		
 		JLabel lblPimps = new JLabel("PIMPS");
+		lblPimps.setBounds(597, 5, 79, 38);
 		lblPimps.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPimps.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 28));
 		panel_2.add(lblPimps);
 		
 		JPanel panel = new JPanel();
-		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.insets = new Insets(0, 0, 5, 0);
-		gbc_panel.fill = GridBagConstraints.BOTH;
-		gbc_panel.gridx = 0;
-		gbc_panel.gridy = 1;
-		contentPane.add(panel, gbc_panel);
+		panel.setBounds(5, 58, 1273, 48);
+		contentPane.add(panel);
+		panel.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Home");
+		lblNewLabel.setBounds(606, 5, 61, 30);
 		lblNewLabel.setToolTipText("");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 22));
 		panel.add(lblNewLabel);
 		
-		JPanel panel_1 = new JPanel();
-		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
-		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
-		gbc_panel_1.fill = GridBagConstraints.BOTH;
-		gbc_panel_1.gridx = 0;
-		gbc_panel_1.gridy = 2;
-		contentPane.add(panel_1, gbc_panel_1);
-		
 		JPanel panel_3 = new JPanel();
-		GridBagConstraints gbc_panel_3 = new GridBagConstraints();
-		gbc_panel_3.fill = GridBagConstraints.BOTH;
-		gbc_panel_3.gridx = 0;
-		gbc_panel_3.gridy = 3;
-		contentPane.add(panel_3, gbc_panel_3);
+		panel_3.setBounds(5, 606, 1273, 88);
+		contentPane.add(panel_3);
+		panel_3.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Home");
-		btnNewButton.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 22));
-		panel_3.add(btnNewButton);
+		JButton btnPatients = new JButton("Patients");
+		btnPatients.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 22));
+		btnPatients.setBounds(1019, 11, 244, 63);
+		panel_3.add(btnPatients);
 		
-		JButton btnNewButton_1 = new JButton("Appointments");
-		btnNewButton_1.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 15));
-		panel_3.add(btnNewButton_1);
+		JButton btnNewButton_8_1 = new JButton("Health History");
+		btnNewButton_8_1.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 22));
+		btnNewButton_8_1.setBounds(765, 11, 244, 63);
+		panel_3.add(btnNewButton_8_1);
 		
-		JButton btnNewButton_2 = new JButton("Chat");
-		btnNewButton_2.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 22));
-		panel_3.add(btnNewButton_2);
+		JButton btnNewButton_8_2 = new JButton("Chat");
+		btnNewButton_8_2.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 22));
+		btnNewButton_8_2.setBounds(511, 11, 244, 63);
+		panel_3.add(btnNewButton_8_2);
 		
-		JButton btnNewButton_3 = new JButton("Health \r\nHistory");
-		btnNewButton_3.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 15));
-		panel_3.add(btnNewButton_3);
+		JButton btnNewButton_8_3 = new JButton("Appointments");
+		btnNewButton_8_3.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 22));
+		btnNewButton_8_3.setBounds(257, 11, 244, 63);
+		panel_3.add(btnNewButton_8_3);
 		
-		JButton btnNewButton_4 = new JButton("Patients");
-		btnNewButton_4.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 22));
-		panel_3.add(btnNewButton_4);
+		JButton btnNewButton_8_4 = new JButton("Home");
+		btnNewButton_8_4.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 22));
+		btnNewButton_8_4.setBounds(10, 11, 237, 63);
+		panel_3.add(btnNewButton_8_4);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(5, 117, 1273, 475);
+		contentPane.add(scrollPane);
+		
+		JPanel panel_1 = new JPanel();
+		scrollPane.setViewportView(panel_1);
+		
+		Box verticalBox = Box.createVerticalBox();
+		panel_1.add(verticalBox);
 	}
 }
